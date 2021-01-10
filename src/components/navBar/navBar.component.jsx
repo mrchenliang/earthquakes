74223f32ed4088808df1d24339473a5bea7249a6
Chen Liang <chenliang@Chens-MacBook-Pro.local>
2021-01-09 19:56:28 -0700

    finished navbar

--- src/components/navBar/navBar.component.jsx
+++ src/components/navBar/navBar.component.jsx
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
 import jsonData from '../../data.json';
+import icon from '../../assets/icon.svg';
 import './navBar.styles.css';
 
 const NavBar = () => {
-    const [data, setData] = useState(JSON.parse(JSON.stringify(jsonData)));
+    const [data] = useState(JSON.parse(JSON.stringify(jsonData)));
     return (
         <nav id = "navBar" className = "navBar">
-            <h1 className="navbar-logo">{data.site.title}<i className="fab fa-react"></i></h1>
-            <div className="menu-icon">
-            </div>
+            <a href = "/" aria-label="Go to home page">
+                <img className = "navBarIcon" src = {icon}/>
+            </a>
+            <h1 className = "navBarTitle">{data?.site?.title || "(Untitled)"}</h1>
+            <a href = "/profile" aria-label="Go to profile page">
+                <hi className = "navBarUser">Welcome {data?.profile?.firstName || " "}</hi>
+            </a>
         </nav>
     );
 };
